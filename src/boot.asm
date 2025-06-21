6caa636031a573c9992efea610e424811007cd7b
suyogbud <suyogbc10@gmail.com>
2025-06-21 00:19:01 -0400

    Entered Protected mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,6 +1,10 @@
 [BITS 16]
 [ORG 0x7c00]
 
+CODE_OFFSET equ 0x8
+
+DATA_OFFSET equ 0x10
+
 start:
   cli; Clear Interrupts
   mov ax, 0x00
@@ -9,36 +13,60 @@ start:
   mov ss, ax
   mov sp, 0x7c00
   sti; Enable Interrupts
-  mov si, msg
 
-print:
-  lodsb; Loads byte at ds:si to AL register and increment SI
-  cmp al, 0
-  je done
-  mov ah, 0x0E
-  int 0x10
-  jmp print
-
-done:
+load_PM:
   cli
-  hlt ;Halting --Stoping CPU execution--
-
-msg: db "Hello World!", 0
+  ldgt[gdt_descriptor]
+  mov eax, cr0
+  or al, 1
+  mov cr0, eax
+  jmp CODE_OFFSET:PModeMain
 
 ;GDT Implementation
 
 gdt_start:
-  db 0x00000000
-  db 0x00000000
+  dd 0x0
+  dd 0x0
 
   ;Code segment descriptor
   dw 0xFFFF     ;Limit
   dw 0x0000     ;Base
   db 0x00       ;Base
-  db 10011010
+  db 10011010b
   db 11001111b ;Flags
+  db 0x00      ;Base
 
+  ;Data segment descriptor
+  dw 0xFFFF     ;Limit
+  dw 0x0000     ;Base
+  db 0x00       ;Base
+  db 10010010b
+  db 11001111b ;Flags
+  db 0x00      ;Base
 
+gdt_end:
+   
+gdt_descriptor:
+  dw gdt_end - gdt_start -1 ; Size of GDT -1
+  dd gdt_start
+
+[BITS 32]
+
+PModeMain:
+  mov ax, DATA_OFFSET
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov ss, ax
+  mov gs, ax
+  mov ebp, 0x9C00
+  mov esp, ebp
+
+  in al, 0x92
+  or al, 2
+  out 0x92, al
+
+  jmp $
 times 510 - ($ - $$) db 0
 
 dw 0xAA55
